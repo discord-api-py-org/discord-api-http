bb18a61cda74fb19357e3412f06c7b95f93794dc
tnt <45692264+tuna2134@users.noreply.github.com>
2022-01-21 20:50:29 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     extras_require=extras,
     packages=packages,
     name="discord-api-types",
-    version="0.0.2",
+    version="0.0.3",
     author="DMS",
     author_email="masato190411@gmail.com",
     description="This is discord-api low wrapper.",
