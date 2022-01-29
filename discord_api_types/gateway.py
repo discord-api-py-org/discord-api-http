2f57c12df4fef4f32471354ca302ffec8d1000c7
tnt <45692264+tuna2134@users.noreply.github.com>
2022-01-29 17:19:41 +0900

    Update gateway.py

--- discord_api_types/gateway.py
+++ discord_api_types/gateway.py
@@ -40,22 +40,23 @@ class KeepAlive(threading.Thread):
                     pass
 
 class DiscordGateway:
-    def __init__(self, ws, token):
+    def __init__(self, ws, token, intents:int = int):
         self.ws = ws
         self.token = token
         self.closed = self.ws.closed
+        self.intents
         
     @classmethod
     async def start_gateway(cls, ws, token, intents = 513):
         self = cls(ws, intents, token)
         return self
 
-    async def login(self, intents):
+    async def login(self):
         payload = {
             "op": 2,
             "d": {
                 "token": self.token,
-                "intents": intents,
+                "intents": self.intents,
                 "properties": {
                     "$os": sys.platform,
                     "$browser": "discord-api.py",
